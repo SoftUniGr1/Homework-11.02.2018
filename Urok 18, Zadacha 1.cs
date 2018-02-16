5390146c995495f758d2bca967ef69af3da017bf
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-02-16 19:18:00 +0200

    Add files via upload

---
+++ Urok 18, Zadacha 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_na_susedni_ednakvi_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    nums[i + 1] = nums[i] + nums[i + 1];
+                    nums.RemoveAt(i);
+                    i = - 1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
